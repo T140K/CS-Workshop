d5224d534d8c2c6a6d73bcd4fdcafff7545d693e
T140K <72129491+T140K@users.noreply.github.com>
2022-12-16 15:48:51 +0100

    fizzbuzz

---
+++ fuzzbuzz/fuzzbuzz/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace fuzzbuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            fuzzbuzz();
+        }
+
+        public static void fuzzbuzz()
+        {
+            Console.WriteLine("Write a number\n");
+                        
+            string? a = Console.ReadLine();
+
+            int p;
+            if (Int32.TryParse(a, out p))
+            { 
+                for (int i = 1; i <= p; i++)
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        Console.WriteLine("fuzzbuzz");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        Console.WriteLine("fuzz");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Console.WriteLine("buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("you have to write a number!");
+                fuzzbuzz();
+            }
+        }
+    }
+}
